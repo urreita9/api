124b6c4f9bb814396ead57166e35b0b0ad753ff1
urreita9 <francisco.urrea9@gmail.com>
2022-04-11 14:41:18 -0300

    first commit

---
+++ src/controllers/controllers.js
@@ -0,0 +1,1 @@
+// const { Pokemon, Type } = require('../db.js');
---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+// module.exports = (sequelize) => {
+// defino el modelo
+// 	sequelize.define(
+// 		'user',
+// 		{
+// 			id: {
+// 				type: DataTypes.UUID,
+// 				defaultValue: DataTypes.UUIDV4,
+// 				primaryKey: true,
+// 			},
+// 			name: {
+// 				type: DataTypes.STRING,
+// 				allowNull: false,
+// 				isUnique: true,
+// 			},
+
+// 		},
+// 		{ logging: false, timestamps: false }
+// 	);
+// };
