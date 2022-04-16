af094a90ee97459c44288b30aac418ee5e849376
matiasangelani <matiasangelani2@gmail.com>
2022-04-16 16:11:26 -0300

    middleware modified for multiple images

--- src/middlewares/transformImage.js
+++ src/middlewares/transformImage.js
@@ -1,15 +1,39 @@
 const { cloudinary } = require('../cloudinary');
 
-const transformImage = async (req, res, next) => {
-  let user = req.body;
-  const { img } = user;
+// PASARLO A MIDDLEWARE PARA IMG DE USER
+// const transformImage = async (req, res, next) => {
+//   let user = req.body;
+//   const { image } = user;
 
-  const uploadedResponse = await cloudinary.uploader.upload(img);
+//   const uploadedResponse = await cloudinary.uploader.upload(image);
+
+//   req.body = {
+//     ...req.body,
+//     image: uploadedResponse.secure_url,
+//   };
+
+//   next();
+// };
+
+// middleware para images de caretaker
+const transformImage = async (req, res, next) => {
+  let caretaker = req.body;
+  let { images } = caretaker;
+
+  const uploadedResponse = images.map(
+    async (image) => await cloudinary.uploader.upload(image)
+  );
+
+  console.log('tatta', uploadedResponse);
+
+  const promisesImages = await Promise.all(uploadedResponse);
 
   req.body = {
     ...req.body,
-    img: uploadedResponse.secure_url,
+    images: promisesImages.map((image) => image.secure_url),
   };
+
+  console.log(req.body);
 
   next();
 };
