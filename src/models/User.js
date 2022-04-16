c4b2d63f612ede51030d84e2a2e3ff1f68b19ed4
matiasangelani <matiasangelani2@gmail.com>
2022-04-15 22:03:21 -0300

    attribute img added to model

--- src/models/User.js
+++ src/models/User.js
@@ -1,43 +1,46 @@
-const { Sequelize, DataTypes } = require("sequelize");
+const { Sequelize, DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-    //defino el modelo
-    sequelize.define(
-        "user",
-        {
-            id: {
-                type: Sequelize.UUID,
-                defaultValue: Sequelize.UUIDV4,
-                primaryKey: true,
-            },
-            email: {
-                type: DataTypes.STRING,
-                unique: true,
-                allowNull: false,
-            },
-            password: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            name: {
-                type: DataTypes.STRING,
-            },
-            lastname: {
-                type: DataTypes.STRING,
-            },
-            address: {
-                type: DataTypes.STRING,
-            },
-            points: {
-                type: DataTypes.FLOAT,
-                defaultValue: 0,
-            },
-            role: {
-                type: DataTypes.ENUM("USER", "ADMIN"),
-                defaultValue: "USER",
-            },
-        },
-        { logging: false, timestamps: false }
-    );
+  //defino el modelo
+  sequelize.define(
+    'user',
+    {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+      },
+      address: {
+        type: DataTypes.STRING,
+      },
+      points: {
+        type: DataTypes.FLOAT,
+        defaultValue: 0,
+      },
+      role: {
+        type: DataTypes.ENUM('USER', 'ADMIN'),
+        defaultValue: 'USER',
+      },
+      img: {
+        type: DataTypes.STRING,
+      },
+    },
+    { logging: false, timestamps: false }
+  );
 };
