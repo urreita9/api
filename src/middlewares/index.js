a629c0daf129313c822bf91e213e7d507b73816f
cicarelligiuliano <cicarelligiuliano@outlook.com.ar>
2022-04-23 15:23:46 -0300

    make it better

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,6 +1,8 @@
-const validarCampos = require("./validar-campos");
-const validarJWT = require("./validar-jwt");
-const validarPermisos = require("./validar-permisos");
+const validarCampos = require('./validar-campos');
+const validarJWT = require('./validar-jwt');
+const validarPermisos = require('./validar-permisos');
+
+//Hola mama
 
 module.exports = {
     ...validarCampos,
