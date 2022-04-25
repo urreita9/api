9c91236dfdd63033050018a42f1fe699c08d3d3f
Mariano <mariano.bustos.moron@gmail.com>
2022-04-25 00:31:45 -0300

    tkm mati

--- src/routes/operations.js
+++ src/routes/operations.js
@@ -1,20 +1,17 @@
-const { Router } = require('express');
-const server = require('../app');
+const { Router } = require("express");
+const server = require("../app");
 const router = Router();
 
 const {
-	createOperation,
-	getOperations,
-	editOperation,
-} = require('../controllers/operations');
+  createOperation,
+  captureOrder,
+  getOperations,
+  editOperation,
+} = require("../controllers/operations");
 
-router.get('/:id', getOperations);
-router.put('/', editOperation);
-router.post('/', createOperation);
-
-
-
-
-
+// router.get("/:id", getOperations);
+// router.put("/", editOperation);
+router.post("/create-order", createOperation);
+router.get("/capture-order", captureOrder);
 
 module.exports = router;
