dc6ee870142348ed9cb1c1e540901cc3ec4b5cf5
matiasangelani <matiasangelani2@gmail.com>
2022-04-29 22:08:16 -0300

    controllers/functions folder created, it contains all extra functions for controllers in operation file

---
+++ src/controllers/functions/operationFunctions.js
@@ -0,0 +1,80 @@
+const { User, Caretaker, Operation, Pet } = require('../../db');
+
+const verifyStatus = (status) => {
+  switch (status) {
+    case 'COMPLETED':
+      return 'APPROVED';
+
+    default:
+      return 'CREATED';
+  }
+};
+
+const searchOperations = async (operations, user) => {
+  user === 'true'
+    ? (operations = await Promise.all(
+        operations.map(async (operation) => {
+          const { caretakerId, petId } = operation;
+
+          const caretaker = await User.findByPk(caretakerId);
+          const pet = await Pet.findByPk(petId);
+
+          return {
+            operation,
+            caretaker,
+            pet,
+          };
+        })
+      ))
+    : (operations = await Promise.all(
+        operations.map(async (operation) => {
+          const { userId, petId } = operation;
+
+          const user = await User.findByPk(userId);
+          const pet = await Pet.findByPk(petId);
+
+          return {
+            operation,
+            user,
+            pet,
+          };
+        })
+      ));
+
+  return operations;
+};
+
+const editStatusOperation = async (operationId) => {
+  try {
+    const operation = await Operation.findByPk(operationId);
+    
+    if (!operation) return { msg: 'Operation does not exist' };
+
+    await operation.update({ status: 'COMPLETED' });
+
+    return operation;
+  } catch (error) {
+    return { msg: 'Edit operation error' };
+  }
+}
+
+const editDispatchOperation = async (operationId) => {
+  try {
+    const operation = await Operation.findByPk(operationId);
+    
+    if (!operation) return { msg: 'Operation does not exist' };
+
+    await operation.update({ status: 'COMPLETED', dispatch: true });
+
+    return operation;
+  } catch (error) {
+    return { msg: 'Edit operation error' }; 
+  }
+}
+
+module.exports ={
+  verifyStatus,
+  searchOperations,
+  editStatusOperation,
+  editDispatchOperation,
+}
