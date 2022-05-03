ca3dde080fb209edb42fd4a061ad1090da7548da
urreita9 <francisco.urrea9@gmail.com>
2022-05-03 00:44:48 -0300

    Giuliano gato. Cambios sockets chat

--- src/controllers/sockets.js
+++ src/controllers/sockets.js
@@ -1,95 +1,88 @@
 const { User, Chat, Message } = require('../db');
 
 const usuarioConectado = async (uid) => {
-    const usuario = await User.findByPk(uid);
-    await usuario.update({ online: true });
+	const usuario = await User.findByPk(uid);
+	await usuario.update({ online: true });
 
-    return usuario;
+	return usuario;
 };
 
 const usuarioDesconectado = async (uid) => {
-    const usuario = await User.findByPk(uid);
-    await usuario.update({ online: false });
+	const usuario = await User.findByPk(uid);
+	await usuario.update({ online: false });
 
-    return usuario;
+	return usuario;
 };
 
-const crearMensaje = async (de, para, mensaje, createAt, chatId) => {
-    try {
-        const msg = await Message.create({
-            de,
-            para,
-            mensaje,
-            createAt,
-            chatId,
-        });
+const crearMensaje = async ({ de, para, mensaje, createAt, chatId }) => {
+	try {
+		const msg = await Message.create({
+			de,
+			para,
+			mensaje,
+			createAt,
+			chatId,
+		});
 
-        return msg;
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
+		return msg;
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
 };
 
+const getAllChatsIO = async (id) => {
+	try {
+		const user = await User.findByPk(id, {
+			attributes: { exclude: ['password'] },
+			include: [
+				{
+					model: Chat,
+					attributes: { exclude: ['User-Chats'] },
+					through: {
+						attributes: [],
+					},
+					include: [
+						{
+							model: User,
+							attributes: ['id', 'name', 'lastname', 'img', 'online'],
+							through: {
+								attributes: [],
+							},
+						},
+					],
+				},
+			],
+		});
 
-const getAllChatsIO = (id) => {
+		const usuario = JSON.parse(JSON.stringify(user));
 
-    try {
-        const user = await User.findByPk(id, {
-            attributes: { exclude: ['password'] },
-            include: [
-                {
-                    model: Chat,
-                    attributes: { exclude: ['User-Chats'] },
-                    through: {
-                        attributes: [],
-                    },
-                    include: [
-                        {
-                            model: User,
-                            attributes: ['id', 'name', 'lastname', 'img'],
-                            through: {
-                                attributes: [],
-                            },
-                        },
-                    ],
-                },
-            ],
-        });
-    
-        const usuario = JSON.parse(JSON.stringify(user));
-    
-        const chats = usuario.chats;
-    
-        const chatsFinal = chats.map((el) => {
-            var user2 = {};
-            el.users.forEach((user) => {
-                if (user.id !== usuario.id) {
-                    user2 = user;
-                }
-            });
-    
-            return {
-                chatId: el.id,
-                user2,
-            };
-        });
+		const chats = usuario.chats;
 
-        return chatsFinal
-        
-    } catch (error) {
-        console.log(error)
-        return false
-        
-    }
-    
+		const chatsFinal = chats.map((el) => {
+			var user2 = {};
+			el.users.forEach((user) => {
+				if (user.id !== usuario.id) {
+					user2 = user;
+				}
+			});
 
-    
-}
+			return {
+				chatId: el.id,
+				user2,
+			};
+		});
+
+		return chatsFinal;
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
+};
 
 module.exports = {
-    usuarioConectado,
-    usuarioDesconectado,
-    crearMensaje,
-    getAllChatsIO,
+	usuarioConectado,
+	usuarioDesconectado,
+	crearMensaje,
+	getAllChatsIO,
 };
--- src/routes/sockets.js
+++ src/routes/sockets.js
@@ -1,45 +1,58 @@
-const { usuarioConectado, usuarioDesconectado, crearMensaje, getAllChatsIO } = require('../controllers/sockets');
+const {
+	usuarioConectado,
+	usuarioDesconectado,
+	crearMensaje,
+	getAllChatsIO,
+} = require('../controllers/sockets');
 const { comprobarJWT } = require('../helpers/generar-jwt');
 
 class Sockets {
-    constructor(io) {
-        this.io = io;
+	constructor(io) {
+		this.io = io;
 
-        this.socketEvents();
-    }
-    socketEvents() {
-        this.io.on('connection', async (socket) => {
-            const token = socket.handshake.query['x-token'];
-            //Comprobar autentificacion
-            const [ok, uid] = await comprobarJWT(token);
+		this.socketEvents();
+	}
+	socketEvents() {
+		this.io.on('connection', async (socket) => {
+			const token = socket.handshake.query['x-token'];
+			//Comprobar autentificacion
+			const [ok, uid] = await comprobarJWT(token);
 
-            if (!ok) {
-                console.log('Socket no identificado');
-                return socket.disconnect();
-            }
+			if (!ok) {
+				console.log('Socket no identificado');
+				return socket.disconnect();
+			}
 
-            await usuarioConectado(uid);
+			await usuarioConectado(uid);
 
-            console.log('Usuario conectado');
+			console.log('Usuario conectado');
 
-            socket.emit('chats', await getAllChatsIO(uid));
+			socket.join(uid);
+			const chats = await getAllChatsIO(uid);
 
-            //Conectar a sala
-            socket.join(uid);
+			socket.emit('chats', chats);
 
-            socket.on('mensaje-personal', async (payload) => {
-                const mensaje = await crearMensaje(payload);
-                this.io.to(payload.para).emit('mensaje-personal', mensaje);
-                this.io.to(payload.de).emit('mensaje-personal', mensaje);
-            });
+			const usersParaActualizar = chats.map((el) => el.user2.id);
 
-            //Desconecion del socket
-            socket.on('disconnect', async () => {
-                console.log('disconnect');
-                await usuarioDesconectado(uid);
-            });
-        });
-    }
+			usersParaActualizar.forEach(async (id) => {
+				this.io.to(id).emit('actualizate-perro', await getAllChatsIO(id));
+			});
+
+			socket.on('personal-message', async (payload) => {
+				const mensaje = await crearMensaje(payload);
+				this.io.to(payload.para).emit('personal-message', mensaje);
+				this.io.to(payload.de).emit('personal-message', mensaje);
+			});
+			//Desconecion del socket
+			socket.on('disconnect', async () => {
+				console.log('disconnect');
+				await usuarioDesconectado(uid);
+				usersParaActualizar.forEach(async (el) => {
+					this.io.to(el).emit('actualizate-perro', await getAllChatsIO(el));
+				});
+			});
+		});
+	}
 }
 
 module.exports = Sockets;
