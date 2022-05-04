5ba8d0a1b391becbd0ee2ea081b676de117158cf
urreita9 <francisco.urrea9@gmail.com>
2022-05-04 16:40:41 -0300

    user banned from superadmin

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -5,88 +5,92 @@ const { generarJWT } = require('../helpers/generar-jwt');
 const bcryptjs = require('bcryptjs');
 
 const login = async (req = request, res = response) => {
-    const { email, password, remember } = req.body;
+	const { email, password, remember } = req.body;
 
-    try {
-        //Verificar si el email existe
-        const user = await User.findOne({ where: { email } });
-        if (!user) {
-            return res.status(400).json({ msg: 'Usuario / Password incorrecto - correo' });
-        }
-        //Verificar contraseña
-        const validPassword = await bcryptjs.compare(password, user.password);
+	try {
+		//Verificar si el email existe
+		const user = await User.findOne({ where: { email } });
+		if (!user) {
+			return res.status(400).json({ msg: 'Wrong user or password' });
+		}
+		//Verificar contraseña
+		const validPassword = await bcryptjs.compare(password, user.password);
 
-        if (!validPassword) {
-            return res.status(400).json({ msg: 'Usuario / Password incorrecto - password' });
-        }
+		if (!validPassword) {
+			return res.status(400).json({ msg: 'Wrong user or password' });
+		}
 
-        var token = '';
-        // Generar JWT
-        if (remember) {
-            token = await generarJWT(user.id);
-        } else {
-            token = await generarJWT(user.id, '24h');
-        }
+		if (user.banned) {
+			return res.status(400).json({ msg: 'This Account has been suspended' });
+		}
 
-        const id = user.id;
-        res.json({ token, id });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            msg: 'Ocurrio un error',
-        });
-    }
+		var token = '';
+		// Generar JWT
+		if (remember) {
+			token = await generarJWT(user.id);
+		} else {
+			token = await generarJWT(user.id, '24h');
+		}
+
+		const id = user.id;
+		res.json({ token, id });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			msg: 'Ocurrio un error',
+		});
+	}
 };
 
 const googleLogin = async (req = request, res = response) => {
-    const { email, name, lastname, img, remember } = req.body;
+	const { email, name, lastname, img, remember } = req.body;
 
-    try {
-        //Verificar si el email existe
-        const user = await User.findOne({ where: { email } });
-        if (!user) {
-            //CREACION DE CUENTA
-            const salt = bcryptjs.genSaltSync();
-            const hashedPassword = bcryptjs.hashSync(name, salt);
+	try {
+		//Verificar si el email existe
+		const user = await User.findOne({ where: { email } });
+		if (!user) {
+			//CREACION DE CUENTA
+			const salt = bcryptjs.genSaltSync();
+			const hashedPassword = bcryptjs.hashSync(name, salt);
 
-            const user = await User.create({
-                name: name.toLowerCase(),
-                lastname: lastname.toLowerCase(),
-                email: email.toLowerCase(),
-                password: hashedPassword,
-                img: img,
-                google: true,
-                passwordsetted: false,
-            });
+			const user = await User.create({
+				name: name.toLowerCase(),
+				lastname: lastname.toLowerCase(),
+				email: email.toLowerCase(),
+				password: hashedPassword,
+				img: img,
+				google: true,
+				passwordsetted: false,
+			});
 
-            // Generar JWT
+			// Generar JWT
 
-            const token = await generarJWT(user.id);
+			const token = await generarJWT(user.id);
 
-            const id = user.id;
-            res.json({ token, id });
-        } else {
-            var token = '';
-            // Generar JWT
-            if (remember) {
-                token = await generarJWT(user.id);
-            } else {
-                token = await generarJWT(user.id, '24h');
-            }
+			const id = user.id;
+			res.json({ token, id });
+		} else {
+			var token = '';
+			// Generar JWT
+			if (remember) {
+				token = await generarJWT(user.id);
+			} else {
+				token = await generarJWT(user.id, '24h');
+			}
 
-            const id = user.id;
-            res.json({ token, id });
-        }
-        //Verificar contraseña
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            msg: 'Ocurrio un error',
-        });
-    }
+			const id = user.id;
+			res.json({ token, id });
+		}
+		//Verificar contraseña
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			msg: 'Ocurrio un error',
+		});
+	}
 };
 
 module.exports = {
-    login,
-    googleLogin,
+	login,
+	googleLogin,
 };
--- src/controllers/User.js
+++ src/controllers/User.js
@@ -6,403 +6,411 @@ const bcryptjs = require('bcryptjs');
 const nodemailer = require('nodemailer');
 
 const getUsers = async (req = request, res = response) => {
-    const users = await User.findAll({
-        attributes: { exclude: ['password'] },
-        include: [
-            {
-                model: Pet,
-            },
-            {
-                model: Caretaker,
-            },
-            {
-                model: Chat,
-                attributes: { exclude: ['User-Chats'] },
-                through: {
-                    attributes: [],
-                },
-                include: [
-                    {
-                        model: User,
-                        attributes: ['id'],
-                        through: {
-                            attributes: [],
-                        },
-                    },
-                ],
-            },
-        ],
-    });
+	const users = await User.findAll({
+		attributes: { exclude: ['password'] },
+		include: [
+			{
+				model: Pet,
+			},
+			{
+				model: Caretaker,
+			},
+			{
+				model: Chat,
+				attributes: { exclude: ['User-Chats'] },
+				through: {
+					attributes: [],
+				},
+				include: [
+					{
+						model: User,
+						attributes: ['id'],
+						through: {
+							attributes: [],
+						},
+					},
+				],
+			},
+		],
+	});
 
-    res.json(users);
+	res.json(users);
 };
 
 const getUsersAdmin = async (req = request, res = response) => {
-    const users = await User.findAll({
-        attributes: { exclude: ['password'] },
-        include: [
-            {
-                model: Pet,
-            },
-            {
-                model: Caretaker,
-            },
-        ],
-    });
+	const users = await User.findAll({
+		attributes: { exclude: ['password'] },
+		include: [
+			{
+				model: Pet,
+			},
+			{
+				model: Caretaker,
+			},
+		],
+	});
 
-    res.json(users);
+	res.json(users);
 };
 
 const getUser = async (req = request, res = response) => {
-    const { id } = req.params;
+	const { id } = req.params;
 
-    const user = await User.findByPk(id, {
-        attributes: { exclude: ['password'] },
-        include: [
-            {
-                model: Pet,
-            },
-            {
-                model: Caretaker,
-            },
-        ],
-    });
+	const user = await User.findByPk(id, {
+		attributes: { exclude: ['password'] },
+		include: [
+			{
+				model: Pet,
+			},
+			{
+				model: Caretaker,
+			},
+		],
+	});
 
-    res.json(user);
+	res.json(user);
 };
 
 const getUserJWT = async (req = request, res = response) => {
-    let { id } = req.params;
-    if (!id) {
-        id = req.header('uid');
-    }
+	let { id } = req.params;
+	if (!id) {
+		id = req.header('uid');
+	}
 
-    const user = await User.findByPk(id, {
-        attributes: { exclude: ['password'] },
-        include: [
-            {
-                model: Pet,
-            },
-            {
-                model: Caretaker,
-                include: [
-                    {
-                        model: Image,
-                    },
-                ],
-            },
-            {
-                model: Chat,
-                attributes: { exclude: ['User-Chats'] },
-                through: {
-                    attributes: [],
-                },
-                include: [
-                    {
-                        model: User,
-                        attributes: ['id', 'name', 'lastname', 'img'],
-                        through: {
-                            attributes: [],
-                        },
-                    },
-                ],
-            },
-        ],
-    });
+	const user = await User.findByPk(id, {
+		attributes: { exclude: ['password'] },
+		include: [
+			{
+				model: Pet,
+			},
+			{
+				model: Caretaker,
+				include: [
+					{
+						model: Image,
+					},
+				],
+			},
+			{
+				model: Chat,
+				attributes: { exclude: ['User-Chats'] },
+				through: {
+					attributes: [],
+				},
+				include: [
+					{
+						model: User,
+						attributes: ['id', 'name', 'lastname', 'img'],
+						through: {
+							attributes: [],
+						},
+					},
+				],
+			},
+		],
+	});
 
-    const usuario = JSON.parse(JSON.stringify(user));
+	const usuario = JSON.parse(JSON.stringify(user));
 
-    const chats = usuario.chats;
+	const chats = usuario.chats;
 
-    const chatsFinal = chats.map((el) => {
-        var user2 = {};
-        el.users.forEach((user) => {
-            if (user.id !== usuario.id) {
-                user2 = user;
-            }
-        });
+	const chatsFinal = chats.map((el) => {
+		var user2 = {};
+		el.users.forEach((user) => {
+			if (user.id !== usuario.id) {
+				user2 = user;
+			}
+		});
 
-        return {
-            chatId: el.id,
-            user2,
-        };
-    });
+		return {
+			chatId: el.id,
+			user2,
+		};
+	});
 
-    usuario.chats = chatsFinal;
+	usuario.chats = chatsFinal;
 
-    res.json(usuario);
+	res.json(usuario);
 };
 
 const createUser = async (req = request, res = response) => {
-    let { name, lastname, email, password } = req.body;
+	let { name, lastname, email, password } = req.body;
 
-    let mailTransporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.gmail,
-            pass: process.env.gmail_pass,
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
-    });
+	let mailTransporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.gmail,
+			pass: process.env.gmail_pass,
+		},
+		tls: {
+			rejectUnauthorized: false,
+		},
+	});
 
-    let details = {
-        from: `PetTrip 🐕🐈 <${process.env.gmail}>`,
-        to: email,
-        subject: 'Welcome to Pettrip! 🐕🐈',
-        text: `Welcome to pettrip, ${name} ${lastname}! you have successfully created your account with the email ${email}.`,
-    };
+	let details = {
+		from: `PetTrip 🐕🐈 <${process.env.gmail}>`,
+		to: email,
+		subject: 'Welcome to Pettrip! 🐕🐈',
+		text: `Welcome to pettrip, ${name} ${lastname}! you have successfully created your account with the email ${email}.`,
+	};
 
-    const user = await User.findOne({
-        where: {
-            email: email.toLowerCase(),
-        },
-    });
+	const user = await User.findOne({
+		where: {
+			email: email.toLowerCase(),
+		},
+	});
 
-    if (user) {
-        return res.status(400).json({ msg: `Email ${email} en uso`, state: false });
-    } else {
-        const salt = bcryptjs.genSaltSync();
-        const hashedPassword = bcryptjs.hashSync(password, salt);
+	if (user) {
+		return res.status(400).json({ msg: `Email already exists `, state: false });
+	} else {
+		const salt = bcryptjs.genSaltSync();
+		const hashedPassword = bcryptjs.hashSync(password, salt);
 
-        const userCreated = await User.create({
-            name: name.toLowerCase(),
-            lastname: lastname.toLowerCase(),
-            email: email.toLowerCase(),
-            password: hashedPassword,
-        });
-        const { id } = userCreated;
-        if (userCreated) {
-            mailTransporter.sendMail(details, (error) => {
-                if (error) {
-                    console.log(error.message);
-                } else {
-                    console.log(`email sent`);
-                }
-            });
-            return res.status(201).json({ state: true });
-        } else {
-            return res.status(500).json({ state: false, msg: 'Something went wrong' });
-        }
-    }
+		const userCreated = await User.create({
+			name: name.toLowerCase(),
+			lastname: lastname.toLowerCase(),
+			email: email.toLowerCase(),
+			password: hashedPassword,
+		});
+		const { id } = userCreated;
+		if (userCreated) {
+			mailTransporter.sendMail(details, (error) => {
+				if (error) {
+					console.log(error.message);
+				} else {
+					console.log(`email sent`);
+				}
+			});
+			return res.status(201).json({ state: true });
+		} else {
+			return res
+				.status(500)
+				.json({ state: false, msg: 'Something went wrong' });
+		}
+	}
 };
 
 const editUser = async (req = request, res = response) => {
-    const { id } = req.params;
-    let { email, points, password, rol, ...resto } = req.body;
+	const { id } = req.params;
+	let { email, points, password, rol, ...resto } = req.body;
 
-    const user = await User.findByPk(id);
-    if (password) {
-        password = bcryptjs.hashSync(password, 10);
+	const user = await User.findByPk(id);
+	if (password) {
+		password = bcryptjs.hashSync(password, 10);
 
-        resto.password = password;
-    }
-    for (i in resto) {
-        if (i !== 'role' && i !== 'password') {
-            resto[i] = resto[i].toLowerCase();
-        } else if (i === 'role') {
-            resto[i] = resto[i].toUpperCase();
-        }
-    }
-    await user.update(resto);
+		resto.password = password;
+	}
+	for (i in resto) {
+		if (i !== 'role' && i !== 'password') {
+			resto[i] = resto[i].toLowerCase();
+		} else if (i === 'role') {
+			resto[i] = resto[i].toUpperCase();
+		}
+	}
+	await user.update(resto);
 
-    res.json(user);
+	res.json(user);
 };
 
 const setPassword = async (req = request, res = response) => {
-    const { id } = req.params;
-    let { password } = req.body;
+	const { id } = req.params;
+	let { password } = req.body;
 
-    const user = await User.findByPk(id);
+	const user = await User.findByPk(id);
 
-    password = bcryptjs.hashSync(password, 10);
+	password = bcryptjs.hashSync(password, 10);
 
-    await user.update({ password, passwordsetted: true });
+	await user.update({ password, passwordsetted: true });
 
-    res.json(user);
+	res.json(user);
 };
 
 const deleteUser = async (req = request, res = response) => {
-    const { id } = req.params;
+	const { id } = req.params;
 
-    const user = await User.findByPk(id);
-    await user.destroy();
+	const user = await User.findByPk(id);
+	await user.destroy();
 
-    res.json(user);
+	res.json(user);
 };
 
 const checkPassword = async (req = request, res = response) => {
-    const { password } = req.body;
-    const id = req.header('uid');
+	const { password } = req.body;
+	const id = req.header('uid');
 
-    const user = await User.findByPk(id);
+	const user = await User.findByPk(id);
 
-    const validPassword = await bcryptjs.compare(password, user.password);
+	const validPassword = await bcryptjs.compare(password, user.password);
 
-    return res.json(validPassword);
+	return res.json(validPassword);
 };
 
 const createSuperAdmin = async (req = request, res = response) => {
-    let { name, lastname, email, password, supperPass } = req.body;
+	let { name, lastname, email, password, supperPass } = req.body;
 
-    const user = await User.findOne({
-        where: {
-            email: email.toLowerCase(),
-        },
-    });
+	const user = await User.findOne({
+		where: {
+			email: email.toLowerCase(),
+		},
+	});
 
-    if (supperPass && supperPass === process.env.SUPERADMIN_PASS) {
-        if (user) {
-            return res.status(400).json({ msg: `Email ${email} en uso`, state: false });
-        } else {
-            const salt = bcryptjs.genSaltSync();
-            const hashedPassword = bcryptjs.hashSync(password, salt);
+	if (supperPass && supperPass === process.env.SUPERADMIN_PASS) {
+		if (user) {
+			return res
+				.status(400)
+				.json({ msg: `Email ${email} en uso`, state: false });
+		} else {
+			const salt = bcryptjs.genSaltSync();
+			const hashedPassword = bcryptjs.hashSync(password, salt);
 
-            const userCreated = await User.create({
-                name: name.toLowerCase(),
-                lastname: lastname.toLowerCase(),
-                email: email.toLowerCase(),
-                password: hashedPassword,
-                role: 'SUPER_ADMIN',
-            });
+			const userCreated = await User.create({
+				name: name.toLowerCase(),
+				lastname: lastname.toLowerCase(),
+				email: email.toLowerCase(),
+				password: hashedPassword,
+				role: 'SUPER_ADMIN',
+			});
 
-            return res.status(201).json({ state: true });
-        }
-    } else {
-        res.status(400).json({ state: false, msg: 'Wrong credentials' });
-    }
+			return res.status(201).json({ state: true });
+		}
+	} else {
+		res.status(400).json({ state: false, msg: 'Wrong credentials' });
+	}
 };
 
 const createAdmin = async (req = request, res = response) => {
-    let { name, lastname, email, password } = req.body;
+	let { name, lastname, email, password } = req.body;
 
-    const user = await User.findOne({
-        where: {
-            email: email.toLowerCase(),
-        },
-    });
+	const user = await User.findOne({
+		where: {
+			email: email.toLowerCase(),
+		},
+	});
 
-    if (user) {
-        return res.status(400).json({ msg: `Email ${email} en uso`, state: false });
-    } else {
-        const salt = bcryptjs.genSaltSync();
-        const hashedPassword = bcryptjs.hashSync(password, salt);
+	if (user) {
+		return res.status(400).json({ msg: `Email ${email} en uso`, state: false });
+	} else {
+		const salt = bcryptjs.genSaltSync();
+		const hashedPassword = bcryptjs.hashSync(password, salt);
 
-        const userCreated = await User.create({
-            name: name.toLowerCase(),
-            lastname: lastname.toLowerCase(),
-            email: email.toLowerCase(),
-            password: hashedPassword,
-            role: 'ADMIN',
-        });
+		const userCreated = await User.create({
+			name: name.toLowerCase(),
+			lastname: lastname.toLowerCase(),
+			email: email.toLowerCase(),
+			password: hashedPassword,
+			role: 'ADMIN',
+		});
 
-        return res.status(201).json({ state: true });
-    }
+		return res.status(201).json({ state: true });
+	}
 };
 
 const beAdmin = async (req = request, res = response) => {
-    const { userId } = req.body;
+	const { userId } = req.body;
 
-    const user = await User.findByPk(userId);
+	const user = await User.findByPk(userId);
 
-    if (user.role !== 'SUPER_ADMIN') {
-        await user.update({ role: 'ADMIN' });
+	if (user.role !== 'SUPER_ADMIN') {
+		await user.update({ role: 'ADMIN' });
 
-        res.json({ state: true });
-    } else {
-        res.json({
-            state: false,
-            msg: 'No puede cambiar el Rol de un SUPER_ADMIN',
-        });
-    }
+		res.json({ state: true });
+	} else {
+		res.json({
+			state: false,
+			msg: 'No puede cambiar el Rol de un SUPER_ADMIN',
+		});
+	}
 };
 
 const beUser = async (req = request, res = response) => {
-    const { userId } = req.body;
+	const { userId } = req.body;
 
-    const user = await User.findByPk(userId);
+	const user = await User.findByPk(userId);
 
-    if (user.role !== 'SUPER_ADMIN') {
-        await user.update({ role: 'USER' });
+	if (user.role !== 'SUPER_ADMIN') {
+		await user.update({ role: 'USER' });
 
-        res.json({ state: true });
-    } else {
-        res.json({
-            state: false,
-            msg: 'No puede cambiar el Rol de un SUPER_ADMIN',
-        });
-    }
+		res.json({ state: true });
+	} else {
+		res.json({
+			state: false,
+			msg: 'No puede cambiar el Rol de un SUPER_ADMIN',
+		});
+	}
 };
 
 const banUser = async (req = request, res = response) => {
-    const { userId } = req.body;
-    const validUser = req.validUser;
-    const user = await User.findByPk(userId);
+	const { userId } = req.body;
+	const validUser = req.validUser;
+	const user = await User.findByPk(userId);
 
-    if (validUser.role === 'SUPER_ADMIN') {
-        if (user.role === 'SUPER_ADMIN') {
-            res.status(400).json({ state: false, msg: 'No puede aplicar ban a un SUPER_ADMIN' });
-        } else {
-            await user.update({ banned: true });
+	if (validUser.role === 'SUPER_ADMIN') {
+		if (user.role === 'SUPER_ADMIN') {
+			res
+				.status(400)
+				.json({ state: false, msg: 'No puede aplicar ban a un SUPER_ADMIN' });
+		} else {
+			await user.update({ banned: true });
 
-            res.json({ state: true });
-        }
-    } else if (validUser.role === 'ADMIN') {
-        if (user.role === 'SUPER_ADMIN' || user.role === 'ADMIN') {
-            res.status(400).json({
-                state: false,
-                msg: 'No tiene permisos para aplicar ban a este usuario por jerarquia',
-            });
-        } else {
-            await user.update({ banned: true });
+			res.json({ state: true });
+		}
+	} else if (validUser.role === 'ADMIN') {
+		if (user.role === 'SUPER_ADMIN' || user.role === 'ADMIN') {
+			res.status(400).json({
+				state: false,
+				msg: 'No tiene permisos para aplicar ban a este usuario por jerarquia',
+			});
+		} else {
+			await user.update({ banned: true });
 
-            res.json({ state: true });
-        }
-    }
+			res.json({ state: true });
+		}
+	}
 };
 
 const unBanUser = async (req = request, res = response) => {
-    const { userId } = req.body;
-    const validUser = req.validUser;
-    const user = await User.findByPk(userId);
+	const { userId } = req.body;
+	const validUser = req.validUser;
+	const user = await User.findByPk(userId);
 
-    if (validUser.role === 'SUPER_ADMIN') {
-        if (user.role === 'SUPER_ADMIN') {
-            res.status(400).json({ state: false, msg: 'No puede aplicar unban a un SUPER_ADMIN' });
-        } else {
-            await user.update({ banned: false });
+	if (validUser.role === 'SUPER_ADMIN') {
+		if (user.role === 'SUPER_ADMIN') {
+			res
+				.status(400)
+				.json({ state: false, msg: 'No puede aplicar unban a un SUPER_ADMIN' });
+		} else {
+			await user.update({ banned: false });
 
-            res.json({ state: true });
-        }
-    } else if (validUser.role === 'ADMIN') {
-        if (user.role === 'SUPER_ADMIN' || user.role === 'ADMIN') {
-            res.status(400).json({
-                state: false,
-                msg: 'No tiene permisos para aplicar unban a este usuario por jerarquia',
-            });
-        } else {
-            await user.update({ banned: false });
+			res.json({ state: true });
+		}
+	} else if (validUser.role === 'ADMIN') {
+		if (user.role === 'SUPER_ADMIN' || user.role === 'ADMIN') {
+			res.status(400).json({
+				state: false,
+				msg: 'No tiene permisos para aplicar unban a este usuario por jerarquia',
+			});
+		} else {
+			await user.update({ banned: false });
 
-            res.json({ state: true });
-        }
-    }
+			res.json({ state: true });
+		}
+	}
 };
 
 module.exports = {
-    getUsers,
-    getUser,
-    getUserJWT,
-    createUser,
-    editUser,
-    deleteUser,
-    checkPassword,
-    createSuperAdmin,
-    createAdmin,
-    beAdmin,
-    beUser,
-    banUser,
-    unBanUser,
-    getUsersAdmin,
-    setPassword,
+	getUsers,
+	getUser,
+	getUserJWT,
+	createUser,
+	editUser,
+	deleteUser,
+	checkPassword,
+	createSuperAdmin,
+	createAdmin,
+	beAdmin,
+	beUser,
+	banUser,
+	unBanUser,
+	getUsersAdmin,
+	setPassword,
 };
